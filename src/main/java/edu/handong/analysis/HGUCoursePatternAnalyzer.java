4b8a9e015c111bf0828c69befebd9f1b8a246009
chaerin <chaerin@172.17.200.74>
2019-06-01 20:01:38 +0900

    add_cli

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -1,17 +1,34 @@
 package edu.handong.analysis;
 
 import java.util.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.io.*;
+
+
 
 import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
 import edu.handong.analysis.utils.NotEnoughArgumentException;
 import edu.handong.analysis.utils.Utils;
 
-public class HGUCoursePatternAnalyzer {
 
+import org.apache.commons.cli.CommandLine;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+
+
+
+
+public class HGUCoursePatternAnalyzer {
+	String path;
+	boolean verbose;
+	boolean help;
+	boolean full;
+	
 	private HashMap<String, Student> students;
 
 	/**
@@ -21,6 +38,32 @@ public class HGUCoursePatternAnalyzer {
 	 * @param args
 	 */
 	public void run(String[] args) {
+		
+		
+
+
+		Options options = createOptions();
+		if(parseOptions(options, args)){
+			if (help){
+				System.out.println("!!!");
+				printHelp(options);
+				return;
+			}
+			
+			// path is required (necessary) data so no need to have a branch.
+			System.out.println("You provided \"" + path + "\" as the value of the option p");
+			
+			// TODO show the number of files in the path
+			
+			if(verbose) {
+		
+				System.out.println("");
+				System.out.println("Your program is terminated. (This message is shown because you turned on -v option!");
+			}
+		
+		}
+		
+	/*
 		try {
 			// when there are not enough arguments from CLI, it throws the
 			// NotEnoughArgmentException which must be defined by you.
@@ -31,6 +74,7 @@ public class HGUCoursePatternAnalyzer {
 			System.exit(0);
 		}
 
+*/
 		String dataPath = args[0]; // csv file to be analyzed
 		String resultPath = args[1]; // the file path where the results are saved.
 		ArrayList<String> lines = Utils.getLines(dataPath, true);
@@ -47,6 +91,61 @@ public class HGUCoursePatternAnalyzer {
 		// Write a file (named like the value of resultPath) with linesTobeSaved.
 		Utils.writeAFile(linesToBeSaved, resultPath);
 	}
+
+	
+
+
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+
+			CommandLine cmd = parser.parse(options, args);
+
+			path = cmd.getOptionValue("p");
+			verbose = cmd.hasOption("v");
+			help = cmd.hasOption("h");
+
+		} catch (Exception e) {
+			printHelp(options);
+			System.out.println("??");
+			return false;
+		}
+
+		return true;
+	}
+
+	
+private Options createOptions() {
+	Options options = new Options();
+
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("p").longOpt("path")
+			.desc("Set a path of a directory or a file to display")
+			.hasArg()
+			.argName("Path name to display")
+			.required()
+			.build());
+
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("v").longOpt("verbose")
+			.desc("Display detailed messages!")
+			//.hasArg()     // this option is intended not to have an option value but just an option
+			.argName("verbose option")
+			//.required() // this is an optional option. So disabled required().
+			.build());
+	
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("h").longOpt("help")
+	        .desc("Help")
+	        .build());
+	
+
+	return options;
+}
+
+
+
 
 	/**
 	 * This method create HashMap<String,Student> from the data csv file. Key is a
@@ -123,4 +222,18 @@ public class HGUCoursePatternAnalyzer {
 		}
 		return result;// do not forget to return a proper variable.
 	}
+	
+	
+	
+	
+	private void printHelp(Options options) {
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "CLI test program";
+		String footer ="\nPlease report issues at https://github.com/lifove/CLIExample/issues";
+		formatter.printHelp("CLIExample", header, options, footer, true);
+	}
+
 }
+
+
